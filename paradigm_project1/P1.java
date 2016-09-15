37b9365519e1684ae1953063945db52f23c9a85a
Vidda <viddamaowj@hotmail.com>
2016-09-15 16:16:23 -0400

    modified file

---
+++ paradigm_project1/P1.java
@@ -0,0 +1,43 @@
+package paradigm_project1;
+
+public class P1 {
+	private static void quicksort(int[] arr,int start,int end){
+		int key = arr[start];  
+		int i = start;    
+		int j = end;  
+		while (i < j) {  
+			while (arr[j] >= key && i < j) {  
+				j--;  
+			}  
+			arr[i] = arr[j];  
+			while (arr[i] <= key && i < j) {  
+				i++;  
+			}  
+			arr[j] = arr[i];  
+		}  
+		arr[i] = key;  
+		if (i - 1 > start) {  
+			quicksort(arr, start, i - 1);  
+		}  
+		if (i + 1 < end) {  
+			quicksort(arr, i + 1, end);  
+		}  
+	}  
+
+
+	public static void main(String args[]){
+		int[] data =  new int[1000];
+		for (int i = 0; i < data.length; i++) {  
+			data[i] = (int) Math.round(Math.random()*10000);
+		}  
+		quicksort(data,0,data.length - 1);
+
+		for (int i = 0; i < data.length; i++) {  
+			System.out.print(data[i] + " ");  
+			if (i % 10 == 9){
+				System.out.println();
+			}
+		}  
+	}
+}
+
---
+++ paradigm_project1/P2.java
@@ -0,0 +1,53 @@
+package paradigm_project1;
+
+import java.util.Stack;
+
+public class P2 {
+	private static void quicksort(int[] arr){
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(0);
+		s.push(arr.length-1);
+		while (!s.isEmpty()){	
+			int right = s.pop();
+			int left = s.pop();
+			if (right<=left){
+				continue;
+			}
+			int i = partition(arr,left,right);
+			s.push(left);
+			s.push(i-1);
+
+			s.push(i+1);
+			s.push(right);
+		}
+	}
+
+	private static int partition(int[] arr,int left,int right){
+		int i = left,j = right,pivot = arr[left];
+		while (i<j){
+			for(;i<j && arr[j]>=pivot;j--); 
+			arr[i] = arr[j];                    
+			for(;i<j && arr[i]<=pivot;i++); 
+			arr[j] = arr[i];     
+		}
+		arr[i] = pivot;
+		return i;
+
+	}
+
+	public static void main(String args[]){
+		int[] data =  new int[1000];
+		for (int i = 0; i < data.length; i++) {  
+			data[i] = (int) Math.round(Math.random()*10000);
+		}  
+		quicksort(data);
+
+		for (int i = 0; i < data.length; i++) {  
+			System.out.print(data[i] + " ");  
+			if (i % 10 == 9){
+				System.out.println();
+			}
+		}  
+
+	}
+}
